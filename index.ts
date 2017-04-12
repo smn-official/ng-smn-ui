16590f9d1749ad1ff1072d0d125fb90ae0516f68
Antonio <antonio@engsolutions.com.br>
2017-04-12 17:56:35 -0300

    Primeiro modulo

--- index.ts
+++ index.ts
@@ -1,35 +1,24 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SampleComponent } from './src/sample.component';
-import { SampleDirective } from './src/sample.directive';
-import { SamplePipe } from './src/sample.pipe';
-import { SampleService } from './src/sample.service';
-
-export * from './src/sample.component';
-export * from './src/sample.directive';
-export * from './src/sample.pipe';
-export * from './src/sample.service';
+import { ColorDirective } from './src/directives/color.directive';
+export * from './src/directives/color.directive';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [
-    SampleComponent,
-    SampleDirective,
-    SamplePipe
+    ColorDirective,
   ],
   exports: [
-    SampleComponent,
-    SampleDirective,
-    SamplePipe
+    ColorDirective,
   ]
 })
-export class SampleModule {
+export class SMNUI4Module {
   static forRoot(): ModuleWithProviders {
     return {
-      ngModule: SampleModule,
-      providers: [SampleService]
+      ngModule: SMNUI4Module,
+      providers: []
     };
   }
 }
