48b5a62fdc878f74b9cf0670281086527898ae81
keven <kevenx97ferreira@gmail.com>
2017-04-13 14:55:34 -0300

    add styles

---
+++ src/form/input/input.directive.ts
@@ -0,0 +1,11 @@
+// import { Directive, Renderer, ViewChild, ElementRef } from '@angular/core';
+
+// @Directive({
+// 	selector: 'uiInputContainer'
+// })
+// export class uiInputContainer{
+// 	@ViewChild('uiInputContainer') child: ElementRef 
+// 	constructor(private render: Renderer){
+		
+// 	}
+// }
