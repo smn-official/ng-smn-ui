2aa3b79b47a16d971db6d321dda764c80c7c1741
Antonio <antonio@engsolutions.com.br>
2017-04-13 09:03:11 -0300

    correção de boas práticas TS

--- src/directives/color.directive.ts
+++ src/directives/color.directive.ts
@@ -5,44 +5,40 @@ import { Directive, Attribute, ElementRef, Renderer } from '@angular/core';
 })
 export class ColorDirective {
 
-    /**
-     * 
-     * @param appColor [Hex, porcentagem?]
-     * Caso informado um paramêtro [hexToRgb]
-     * Caso informado dois oaramêtros [isBright] 
-     */
-    constructor( @Attribute("appColor") appColor: any, private el: ElementRef, private render: Renderer) {
+    // @param appColor [Hex, porcentagem?]
+    // Caso informado um paramêtro [hexToRgb]
+    // Caso informado dois oaramêtros [isBright]
+    constructor( @Attribute('appColor') appColor: any, private el: ElementRef, private render: Renderer) {
         if (appColor == null) {
-            console.error("(SMNUI4) Erro na quantidade de atributos - [Diretiva appColor]");
+            console.error('(SMNUI4) Erro na quantidade de atributos - [Diretiva appColor]');
             return;
         }
         let split = appColor.split(',');
-        if (appColor == null || split.length != 2) {
-            console.error("(SMNUI4) Erro na quantidade de atributos - [Diretiva appColor]");
+        if (appColor == null || split.length !== 2) {
+            console.error('(SMNUI4) Erro na quantidade de atributos - [Diretiva appColor]');
             return;
         }
-
         if (split[1] == null) {
             this.render.setElementStyle(this.el.nativeElement, 'color', this.hexToRgb(split[0]));
+            return;
         }
-        else {
-            this.render.setElementStyle(this.el.nativeElement, 'color', this.isBright(split[0], split[1]));
-        }
+        this.render.setElementStyle(this.el.nativeElement, 'color', this.isBright(split[0], split[1]));
     }
 
     private isBright(hex: any, minDarkPerc: any): any {
-        var color = this.hexToRgb(hex);
-        if (!color)
+        let color = this.hexToRgb(hex);
+        if (!color) {
             return false;
+        }
         // Contando a luminosidade perceptiva
         // O olho humano favorece a cor verde
-        var luminosityPerc = 1 - (0.299 * color.r + 0.587 * color.g + 0.114 * color.b) / 255;
+        let luminosityPerc = 1 - (0.299 * color.r + 0.587 * color.g + 0.114 * color.b) / 255;
         return (luminosityPerc < (minDarkPerc || 0.3));
     }
 
     private hexToRgb(hex: any): any {
-        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-        return result ? {
+        let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+        return result == null ? {
             r: parseInt(result[1], 16),
             g: parseInt(result[2], 16),
             b: parseInt(result[3], 16)
