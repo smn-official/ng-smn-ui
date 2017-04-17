4dea4a61e50c2031e156b484c527f053496f27dc
keven <kevenx97ferreira@gmail.com>
2017-04-17 18:29:29 -0300

    atualização input

--- src/form/input/input.component.ts
+++ src/form/input/input.component.ts
@@ -1,33 +1,42 @@
-import { Component, Attribute, OnInit  } from '@angular/core'
+import { Component, Attribute } from '@angular/core'
 
 @Component({
-	selector: 'ui-input',
-	template: require('./input.component.html'), 
+	selector: 'ui-input' + 'ui-textarea',
+	template: require('./input.component.html') + require('./textarea.component.html'), 
 	styles: [require('./input.component.scss')]
 })
-export class inputComponent implements OnInit {
-	_label: string = '';
-	_type: string = 'text';
-	_name: string = '';
-	_value: string = '';
-	_disabled: boolean = false;
-	_required: any;
+export class inputComponent {
+	label: string;
+	type: string = 'text';
+	name: string;
+	value: string;
+	disabled: boolean = false;
+	required: boolean = false;
+	placeholder: string;
+	maxlength: string;
+	readonly: boolean = false;
+	ngModel: any;
 	constructor(
 		@Attribute('label') label: string,
 		@Attribute('type') type: string,
 		@Attribute('name') name: string,
 		@Attribute('value') value: string,
 		@Attribute('disabled') disabled: boolean,
-		@Attribute('required') required: any,
+		@Attribute('required') required: boolean,
+		@Attribute('placeholder') placeholder: string,
+		@Attribute('maxlength') maxlength: string,
+		@Attribute('readonly') readonly: boolean,
+		@Attribute('ngModel') ngModel: any,
 	){
-		this._label = label;
-		this._type = type;
-		this._name = name;
-		this._value = value;
-		this._disabled = disabled;
-		this._required;
+		this.label = label;
+		this.type = type;
+		this.name = name;
+		this.value = value;
+		this.disabled = disabled;
+		this.required = required;
+		this.placeholder = placeholder;
+		this.maxlength = maxlength;
+		this.readonly = readonly;
+		this.ngModel = ngModel;
 	}
-
-	ngOnInit() {
-    }
 }
