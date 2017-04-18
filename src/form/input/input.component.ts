ce6e915eddba97d79b9b2f92608f7e2a6300d7b9
Antonio <antonio@engsolutions.com.br>
2017-04-18 10:08:36 -0300

    nova estrutura de input

--- src/form/input/input.component.ts
+++ src/form/input/input.component.ts
@@ -1,42 +1,13 @@
 import { Component, Attribute } from '@angular/core'
 
 @Component({
-	selector: 'ui-input' + 'ui-textarea',
-	template: require('./input.component.html') + require('./textarea.component.html'), 
+	selector: 'ui-input',
+	template: require('./input.component.html'),
 	styles: [require('./input.component.scss')]
 })
 export class inputComponent {
 	label: string;
-	type: string = 'text';
-	name: string;
-	value: string;
-	disabled: boolean = false;
-	required: boolean = false;
-	placeholder: string;
-	maxlength: string;
-	readonly: boolean = false;
-	ngModel: any;
-	constructor(
-		@Attribute('label') label: string,
-		@Attribute('type') type: string,
-		@Attribute('name') name: string,
-		@Attribute('value') value: string,
-		@Attribute('disabled') disabled: boolean,
-		@Attribute('required') required: boolean,
-		@Attribute('placeholder') placeholder: string,
-		@Attribute('maxlength') maxlength: string,
-		@Attribute('readonly') readonly: boolean,
-		@Attribute('ngModel') ngModel: any,
-	){
+	constructor( @Attribute('label') label: string) {
 		this.label = label;
-		this.type = type;
-		this.name = name;
-		this.value = value;
-		this.disabled = disabled;
-		this.required = required;
-		this.placeholder = placeholder;
-		this.maxlength = maxlength;
-		this.readonly = readonly;
-		this.ngModel = ngModel;
 	}
 }
