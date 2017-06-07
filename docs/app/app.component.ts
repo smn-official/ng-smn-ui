738b833cb91dfaaafe1bb40580a8319baf18cd27
Mario Santos <mariocsants@gmail.com>
2017-06-07 17:43:29 -0300

    Aceitando node

--- docs/app/app.component.ts
+++ docs/app/app.component.ts
@@ -2,12 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: `        
-            <ui-input-container>
-                <input type="text" uiInput [(ngModel)]="teste" name="teste">
-                <label class="teste">teste</label>
-            </ui-input-container>
-        `
+    templateUrl: 'app.component.html'
 })
 export class AppComponent {
 }
