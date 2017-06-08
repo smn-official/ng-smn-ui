c6d415d79205a9db0aa88e4a70337c31d95f13eb
Mario Santos <mariocsants@gmail.com>
2017-06-08 08:33:06 -0300

    Alterações no app.component

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -34,18 +34,12 @@ module.exports = {
             },
             {
                 test: /\.scss$/,
-                use: [{
-                    loader: "style-loader" // creates style nodes from JS strings
-                }, {
-                    loader: "css-loader" // translates CSS into CommonJS
-                }, {
-                    loader: "sass-loader" // compiles Sass to CSS
-                }]
+                loaders: ['style-loader', 'css-loader', 'sass-loader', 'raw-loader']
             },
             {
-                test: /\.css$/,
+                test: /\.(css|scss)$/,
                 exclude: helpers.root('docs', 'app'),
-                loader: ExtractTextPlugin.extract({ fallbackLoader: 'style-loader', loader: ['css-loader', 'sass-loader'] })
+                loader: ExtractTextPlugin.extract({ fallbackLoader: 'style-loader', loader: 'css-loader?sourceMap' })
             },
             {
                 test: /\.css$/,
--- docs/app/app.component.ts
+++ docs/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    templateUrl: 'app.component.html'
+    templateUrl: 'app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
 }
