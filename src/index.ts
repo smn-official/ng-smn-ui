3c824a746ce519156ff71655c05111138ee2d023
Ítalo Andrade <italo@smn.com.br>
2017-06-08 14:46:17 -0300

    Novo componente toolbar

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,25 @@
-import { NgModule, ModuleWithProviders, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import {NgModule, ModuleWithProviders, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterModule} from '@angular/router';
 
 // Components
-import { UiInputContainerComponent } from './form/input/input-container.component';
-import { UiInputDirective } from './form/input/input.directive';
-import { OptionComponent } from './form/option/option.component';
-import { MainMenuComponent } from './struct/main-menu/main-menu.component';
-import { MenuListComponent } from './struct/main-menu/menu-list/menu-list.component';
-import { MenuItemComponent } from './struct/main-menu/menu-item/menu-item.component';
-import { TranslateComponent } from './animations/translate.component';
-import { ContextMenuComponent } from './components/context-menu/context-menu.component';
+import {UiInputContainerComponent} from './form/input/input-container.component';
+import {UiInputDirective} from './form/input/input.directive';
+import {OptionComponent} from './form/option/option.component';
+import {MainMenuComponent} from './struct/main-menu/main-menu.component';
+import {MenuListComponent} from './struct/main-menu/menu-list/menu-list.component';
+import {MenuItemComponent} from './struct/main-menu/menu-item/menu-item.component';
+import {TranslateComponent} from './animations/translate.component';
+import {ContextMenuComponent} from './components/context-menu/context-menu.component';
+import {ToolbarComponent} from './toolbar/toolbar.component';
 
 // Pipes
 // import { DatePipe } from './filters/date.pipe';
 // import { UnaccentPipe } from './filters/unaccent.pipe';
 
 // Diretivas
-import { AutofocusDirective } from './utils/autofocus/autofocus.directive';
-import { PerceptiveDirective } from './perceptive/perceptive.directive';
+import {AutofocusDirective} from './utils/autofocus/autofocus.directive';
+import {PerceptiveDirective} from './perceptive/perceptive.directive';
 
 export * from './form/input/input-container.component';
 export * from './form/input/input.directive';
@@ -28,48 +29,51 @@ export * from './struct/main-menu/menu-list/menu-list.component';
 export * from './struct/main-menu/menu-item/menu-item.component';
 export * from './animations/translate.component';
 export * from './components/context-menu/context-menu.component';
+export * from './toolbar/toolbar.component';
 
 @NgModule({
-	imports: [
-		CommonModule,
-		RouterModule
-	],
-	declarations: [
-		UiInputContainerComponent,
-		UiInputDirective,
-		OptionComponent,
-		MainMenuComponent,
-		MenuListComponent,
-		MenuItemComponent,
-		TranslateComponent,
-		ContextMenuComponent,
-		PerceptiveDirective,
-		AutofocusDirective
-		// UnaccentPipe,
-		// DatePipe
-	],
-	exports: [
-		UiInputContainerComponent,
-		UiInputDirective,
-		OptionComponent,
-		MainMenuComponent,
-		MenuListComponent,
-		MenuItemComponent,
-		TranslateComponent,
-		ContextMenuComponent,
-		PerceptiveDirective,
-		AutofocusDirective
-		// UnaccentPipe,
-		// DatePipe
-	],
-	schemas: [
-		CUSTOM_ELEMENTS_SCHEMA
-	],
+    imports: [
+        CommonModule,
+        RouterModule
+    ],
+    declarations: [
+        UiInputContainerComponent,
+        UiInputDirective,
+        OptionComponent,
+        MainMenuComponent,
+        MenuListComponent,
+        MenuItemComponent,
+        TranslateComponent,
+        ContextMenuComponent,
+        PerceptiveDirective,
+        AutofocusDirective,
+        ToolbarComponent
+        // UnaccentPipe,
+        // DatePipe
+    ],
+    exports: [
+        UiInputContainerComponent,
+        UiInputDirective,
+        OptionComponent,
+        MainMenuComponent,
+        MenuListComponent,
+        MenuItemComponent,
+        TranslateComponent,
+        ContextMenuComponent,
+        PerceptiveDirective,
+        AutofocusDirective,
+        ToolbarComponent
+        // UnaccentPipe,
+        // DatePipe
+    ],
+    schemas: [
+        CUSTOM_ELEMENTS_SCHEMA
+    ],
 })
 export class SMNUIModule {
-	static forRoot(): ModuleWithProviders {
-		return {
-			ngModule: SMNUIModule,
-		};
-	}
+    static forRoot(): ModuleWithProviders {
+        return {
+            ngModule: SMNUIModule,
+        };
+    }
 }
