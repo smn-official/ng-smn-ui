35da1f9f007493158e3a29c9d851ad060fbd714f
Ítalo Andrade <italo@smn.com.br>
2017-06-09 16:41:14 -0300

    Implementando componente Toolbar

---
+++ src/app/smn-ui/toolbar/toolbar.component.ts
@@ -0,0 +1,16 @@
+import {Component, AfterViewInit} from '@angular/core';
+
+@Component({
+  selector: 'ui-toolbar',
+  templateUrl: './toolbar.component.html',
+  styleUrls: ['./toolbar.component.scss']
+})
+export class UiToolbarComponent implements AfterViewInit {
+
+  constructor() {
+  }
+
+  ngAfterViewInit() {
+  }
+
+}
