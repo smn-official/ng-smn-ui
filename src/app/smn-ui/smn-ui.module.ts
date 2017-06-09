2231ef882a1047db2045c8692bc99928592c91c3
Mario Santos <mariocsants@gmail.com>
2017-06-09 17:29:28 -0300

    Component, Demo: Input; Validator: Max length

--- src/app/smn-ui/smn-ui.module.ts
+++ src/app/smn-ui/smn-ui.module.ts
@@ -1,26 +1,28 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {CommonModule} from '@angular/common';
 
-import { UiInputContainerComponent } from './input/input-container.component';
-import { UiInputDirective } from './input/input.directive';
-import { UiToolbarComponent } from './toolbar/toolbar.component';
+import {UiInputContainerComponent} from './input/input-container.component';
+import {UiInputDirective} from './input/input.directive';
+import {UiToolbarComponent} from './toolbar/toolbar.component';
+
+import {UiMaxlengthDirective} from './format/validators/maxlength.directive';
+
+const lib: any[] = [
+    UiInputContainerComponent,
+    UiInputDirective,
+    UiToolbarComponent,
+    UiMaxlengthDirective
+];
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule
-  ],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  declarations: [
-    UiInputContainerComponent,
-    UiInputDirective,
-    UiToolbarComponent
-  ],
-  exports: [
-    UiInputContainerComponent,
-    UiInputDirective,
-    UiToolbarComponent
-  ]
+    imports: [
+        CommonModule,
+        FormsModule
+    ],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    declarations: [...lib],
+    exports: [...lib]
 })
-export class SMNUIModule { }
+export class SMNUIModule {
+}
