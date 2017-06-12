33a8b47d551c0639957f67950d38d140fba7d26b
Mario Santos <mariocsants@gmail.com>
2017-06-12 15:24:27 -0300

    Struct: Export

---
+++ src/app/demo/demo-card/demo-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'demo-card',
+  templateUrl: './demo-card.component.html',
+  styleUrls: ['./demo-card.component.scss']
+})
+export class DemoCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
