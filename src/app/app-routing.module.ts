05bba1561964c866ed384b17bd766c8b64472127
Mario Santos <mariocsants@gmail.com>
2017-06-13 10:52:46 -0300

    Upgrade: Ripple in icon

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import {DemoComponent} from './demo/demo.component';
 import {DemoInputComponent} from './demo/demo-input/demo-input.component';
 import {DemoButtonComponent} from './demo/demo-button/demo-button.component';
 import {DemoCardComponent} from './demo/demo-card/demo-card.component';
+import {DemoDatepickerComponent} from './demo/demo-datepicker/demo-datepicker.component';
 import {DemoOptionComponent} from './demo/demo-option/demo-option.component';
 
 const routes: Routes = [{
@@ -13,6 +14,8 @@ const routes: Routes = [{
     children: [
         {path: 'input', component: DemoInputComponent},
         {path: 'button', component: DemoButtonComponent},
+        {path: 'card', component: DemoCardComponent},
+        {path: 'picker', component: DemoDatepickerComponent},
         {path: 'card', component: DemoCardComponent},
         {path: 'option', component: DemoOptionComponent}
     ]
