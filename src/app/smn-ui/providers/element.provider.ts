9f96d66caea9ef2ac52386462765f1415f8cf2c3
Ítalo Andrade <italo@smn.com.br>
2017-06-15 17:08:29 -0300

    Implementando trigger, on e is no UiElement

--- src/app/smn-ui/providers/element.provider.ts
+++ src/app/smn-ui/providers/element.provider.ts
@@ -1,5 +1,28 @@
 import {Injectable} from '@angular/core';
 
+@Injectable()
+export class UiElement {
+    static closest(el, selector): any {
+        return _closest(el, selector);
+    }
+
+    static index(el): any {
+        return _index(el);
+    }
+
+    static trigger(el, eventToTrigger): any {
+        return _trigger(el, eventToTrigger);
+    }
+
+    static on(el, events, listener): any {
+        return _on(el, events, listener);
+    }
+
+    static is(el, selector): any {
+        return _is(el, selector);
+    }
+}
+
 function _closest(el, selector): any {
     /* Source: http://stackoverflow.com/questions/18663941/finding-closest-element-without-jquery */
 
@@ -40,14 +63,27 @@ function _index(el): any {
 
     return count;
 }
-
-@Injectable()
-export class UiElement {
-    closest(el, selector): any {
-        return _closest(el, selector);
+function _trigger(el, eventToTrigger): any {
+    const event = document.createEvent('HTMLEvents');
+    event.initEvent(eventToTrigger, true, false);
+    el.dispatchEvent(event);
+}
+function _on(el, events, listener): any {
+    events = events.split(' ');
+    const iLen = events.length;
+    for (let i = 0; i < iLen; i++) {
+        if (events[i]) {
+            el.addEventListener(events[i], listener, false);
+        }
     }
+}
+function _is(el, selector): any {
+    el = el || {
+            msMatchesSelector: undefined,
+            mozMatchesSelector: undefined,
+            webkitMatchesSelector: undefined,
+            oMatchesSelector: undefined
+        };
 
-    index(el): any {
-        return _index(el);
-    }
+    return (el.matches || el.matchesSelector || el.msMatchesSelector || el.mozMatchesSelector || el.webkitMatchesSelector || el.oMatchesSelector).call(el, selector);
 }
