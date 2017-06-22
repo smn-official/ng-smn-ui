93a37001163e8d4428f2c49e6dae658d1add0b25
Ítalo Andrade <italo@smn.com.br>
2017-06-22 09:14:21 -0300

    Desenvolvendo o data-table

--- src/app/demo/demo-data-tables/demo-data-tables.component.ts
+++ src/app/demo/demo-data-tables/demo-data-tables.component.ts
@@ -9,7 +9,51 @@ import {UiToolbarService} from '../../smn-ui/smn-ui.module';
     styleUrls: ['./demo-data-tables.component.scss']
 })
 export class DemoDataTablesComponent implements OnInit, AfterViewInit, OnDestroy {
+    list: Array<any>;
+
     constructor(private titleService: Title, private toolbarService: UiToolbarService) {
+        this.list = [
+            {
+                name: 'Spider',
+                surname: 'Man',
+                age: Math.floor((Math.random() * 30) + 20)
+            },
+            {
+                name: 'Captain',
+                surname: 'Marvel',
+                age: Math.floor((Math.random() * 30) + 20)
+            },
+            {
+                name: 'Hulk',
+                surname: undefined,
+                age: Math.floor((Math.random() * 30) + 20)
+            },
+            {
+                name: 'Thor',
+                surname: undefined,
+                age: Math.floor((Math.random() * 30) + 20)
+            },
+            {
+                name: 'Iron',
+                surname: 'Man',
+                age: Math.floor((Math.random() * 30) + 20)
+            },
+            {
+                name: 'Luke',
+                surname: 'Cage',
+                age: Math.floor((Math.random() * 30) + 20)
+            },
+            {
+                name: 'Black',
+                surname: 'Widow',
+                age: Math.floor((Math.random() * 30) + 20)
+            },
+            {
+                name: 'Daredevil',
+                surname: 'of Hell\'s Kitchen',
+                age: Math.floor((Math.random() * 30) + 20)
+            }
+        ];
     }
 
     ngOnInit() {
