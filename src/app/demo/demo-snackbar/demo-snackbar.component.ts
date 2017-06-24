68560e96d0f5c7f2f6722a49aa3e564fa090584f
Mario Santos <mariocsants@gmail.com>
2017-06-24 12:15:56 -0300

    Demo: Config demo snackbar

---
+++ src/app/demo/demo-snackbar/demo-snackbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ui-demo-snackbar',
+  templateUrl: './demo-snackbar.component.html',
+  styleUrls: ['./demo-snackbar.component.scss']
+})
+export class DemoSnackbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
