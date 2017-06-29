a378f771845cc2a6f60de389a940c72e4edcc824
Ítalo Andrade <italo@engsolutions.com.br>
2017-06-29 14:08:19 -0300

    Fix: Limpezas e reordenação

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,22 +2,23 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
 import {DemoComponent} from './demo/demo.component';
-import {DemoInputsComponent} from './demo/demo-inputs/demo-inputs.component';
 import {DemoButtonsComponent} from './demo/demo-buttons/demo-buttons.component';
 import {DemoCardsComponent} from './demo/demo-cards/demo-cards.component';
-import {DemoDatePickersComponent} from './demo/demo-date-pickers/demo-date-pickers.component';
-import {DemoSelectionControlsComponent} from './demo/demo-selection-controls/demo-selection-controls.component';
-import {DemoToolbarsComponent} from './demo/demo-toolbars/demo-toolbars.component';
-import {DemoDividersComponent} from './demo/demo-dividers/demo-dividers.component';
-import {DemoLayoutComponent} from './demo/demo-layout/demo-layout.component';
 import {DemoDataTablesComponent} from './demo/demo-data-tables/demo-data-tables.component';
-import {DemoSnackbarComponent} from './demo/demo-snackbar/demo-snackbar.component';
-import {DemoProgressComponent} from './demo/demo-progress/demo-progress.component';
-import {DemoNavDrawerComponent} from './demo/demo-nav-drawer/demo-nav-drawer.component';
+import {DemoDatePickersComponent} from './demo/demo-date-pickers/demo-date-pickers.component';
+import {DemoDialogComponent} from './demo/demo-dialog/demo-dialog.component';
+import {DemoDividersComponent} from './demo/demo-dividers/demo-dividers.component';
 import {DemoHomeComponent} from './demo/demo-home/demo-home.component';
-import {DemoSmartListComponent} from './demo/demo-smart-list/demo-smart-list.component';
+import {DemoInputsComponent} from './demo/demo-inputs/demo-inputs.component';
+import {DemoLayoutComponent} from './demo/demo-layout/demo-layout.component';
 import {DemoMenuComponent} from './demo/demo-menu/demo-menu.component';
+import {DemoNavDrawerComponent} from './demo/demo-nav-drawer/demo-nav-drawer.component';
+import {DemoProgressComponent} from './demo/demo-progress/demo-progress.component';
+import {DemoSelectionControlsComponent} from './demo/demo-selection-controls/demo-selection-controls.component';
+import {DemoSmartListComponent} from './demo/demo-smart-list/demo-smart-list.component';
+import {DemoSnackbarComponent} from './demo/demo-snackbar/demo-snackbar.component';
 import {DemoSubheaderComponent} from './demo/demo-subheader/demo-subheader.component';
+import {DemoToolbarsComponent} from './demo/demo-toolbars/demo-toolbars.component';
 
 const routes: Routes = [
     {
@@ -25,21 +26,22 @@ const routes: Routes = [
         component: DemoComponent,
         children: [
             {path: '', component: DemoHomeComponent},
-            {path: 'inputs', component: DemoInputsComponent},
             {path: 'buttons', component: DemoButtonsComponent},
             {path: 'cards', component: DemoCardsComponent},
-            {path: 'date-pickers', component: DemoDatePickersComponent},
-            {path: 'selection-controls', component: DemoSelectionControlsComponent},
-            {path: 'toolbars', component: DemoToolbarsComponent},
-            {path: 'dividers', component: DemoDividersComponent},
-            {path: 'layout', component: DemoLayoutComponent},
             {path: 'data-tables', component: DemoDataTablesComponent},
-            {path: 'snackbars', component: DemoSnackbarComponent},
-            {path: 'progress', component: DemoProgressComponent},
-            {path: 'nav-drawer', component: DemoNavDrawerComponent},
-            {path: 'smart-list', component: DemoSmartListComponent},
+            {path: 'date-pickers', component: DemoDatePickersComponent},
+            {path: 'dialog', component: DemoDialogComponent},
+            {path: 'dividers', component: DemoDividersComponent},
+            {path: 'inputs', component: DemoInputsComponent},
+            {path: 'layout', component: DemoLayoutComponent},
             {path: 'menus', component: DemoMenuComponent},
+            {path: 'nav-drawer', component: DemoNavDrawerComponent},
+            {path: 'progress', component: DemoProgressComponent},
+            {path: 'selection-controls', component: DemoSelectionControlsComponent},
+            {path: 'smart-list', component: DemoSmartListComponent},
+            {path: 'snackbars', component: DemoSnackbarComponent},
             {path: 'subheader', component: DemoSubheaderComponent},
+            {path: 'toolbars', component: DemoToolbarsComponent},
         ]
     },
     {path: '**', redirectTo: ''}
--- src/app/demo/demo.module.ts
+++ src/app/demo/demo.module.ts
@@ -4,23 +4,24 @@ import {FormsModule} from '@angular/forms';
 import {RouterModule} from '@angular/router';
 import {SMNUIModule} from '../smn-ui/smn-ui.module';
 
-import {DemoComponent} from './demo.component';
-import {DemoInputsComponent} from './demo-inputs/demo-inputs.component';
 import {DemoButtonsComponent} from './demo-buttons/demo-buttons.component';
 import {DemoCardsComponent} from './demo-cards/demo-cards.component';
-import {DemoDatePickersComponent} from './demo-date-pickers/demo-date-pickers.component';
-import {DemoSelectionControlsComponent} from './demo-selection-controls/demo-selection-controls.component';
-import {DemoToolbarsComponent} from './demo-toolbars/demo-toolbars.component';
-import {DemoDividersComponent} from './demo-dividers/demo-dividers.component';
-import {DemoLayoutComponent} from './demo-layout/demo-layout.component';
+import {DemoComponent} from './demo.component';
 import {DemoDataTablesComponent} from './demo-data-tables/demo-data-tables.component';
-import {DemoSnackbarComponent} from './demo-snackbar/demo-snackbar.component';
-import {DemoProgressComponent} from './demo-progress/demo-progress.component';
-import {DemoNavDrawerComponent} from './demo-nav-drawer/demo-nav-drawer.component';
+import {DemoDatePickersComponent} from './demo-date-pickers/demo-date-pickers.component';
+import {DemoDialogComponent} from './demo-dialog/demo-dialog.component';
+import {DemoDividersComponent} from './demo-dividers/demo-dividers.component';
 import {DemoHomeComponent} from './demo-home/demo-home.component';
-import {DemoSmartListComponent} from './demo-smart-list/demo-smart-list.component';
+import {DemoInputsComponent} from './demo-inputs/demo-inputs.component';
+import {DemoLayoutComponent} from './demo-layout/demo-layout.component';
 import {DemoMenuComponent} from './demo-menu/demo-menu.component';
+import {DemoNavDrawerComponent} from './demo-nav-drawer/demo-nav-drawer.component';
+import {DemoProgressComponent} from './demo-progress/demo-progress.component';
+import {DemoSelectionControlsComponent} from './demo-selection-controls/demo-selection-controls.component';
+import {DemoSmartListComponent} from './demo-smart-list/demo-smart-list.component';
+import {DemoSnackbarComponent} from './demo-snackbar/demo-snackbar.component';
 import {DemoSubheaderComponent} from './demo-subheader/demo-subheader.component';
+import {DemoToolbarsComponent} from './demo-toolbars/demo-toolbars.component';
 
 @NgModule({
     imports: [
@@ -30,23 +31,24 @@ import {DemoSubheaderComponent} from './demo-subheader/demo-subheader.component'
         SMNUIModule
     ],
     declarations: [
-        DemoComponent,
-        DemoInputsComponent,
         DemoButtonsComponent,
         DemoCardsComponent,
-        DemoSelectionControlsComponent,
-        DemoToolbarsComponent,
-        DemoDatePickersComponent,
-        DemoDividersComponent,
-        DemoLayoutComponent,
+        DemoComponent,
         DemoDataTablesComponent,
-        DemoSnackbarComponent,
-        DemoProgressComponent,
-        DemoNavDrawerComponent,
+        DemoDatePickersComponent,
+        DemoDialogComponent,
+        DemoDividersComponent,
         DemoHomeComponent,
-        DemoSmartListComponent,
+        DemoInputsComponent,
+        DemoLayoutComponent,
         DemoMenuComponent,
-        DemoSubheaderComponent
+        DemoNavDrawerComponent,
+        DemoProgressComponent,
+        DemoSelectionControlsComponent,
+        DemoSmartListComponent,
+        DemoSnackbarComponent,
+        DemoSubheaderComponent,
+        DemoToolbarsComponent,
     ],
     exports: [DemoComponent],
     schemas: [CUSTOM_ELEMENTS_SCHEMA]
