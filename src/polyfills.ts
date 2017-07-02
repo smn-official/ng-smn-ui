a7572d90d0ac5cbf5faf8697c583bc2d86409a81
Ítalo Andrade <andraditalo@gmail.com>
2017-07-01 22:35:33 -0300

    Fix: Compatibilidade de animações

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -49,12 +49,10 @@ import 'core-js/es7/reflect';
 // import 'web-animations-js';  // Run `npm install --save web-animations-js`.
 
 
-
 /***************************************************************************************************
  * Zone JS is required by Angular itself.
  */
 import 'zone.js/dist/zone';  // Included with Angular CLI.
-
 
 
 /***************************************************************************************************
@@ -70,3 +68,9 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
  * Need to import at least one locale-data with intl.
  */
 // import 'intl/locale-data/jsonp/en';
+
+/**
+ * Animation compatibility
+ */
+
+import 'web-animations-js';
