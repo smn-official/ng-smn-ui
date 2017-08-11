c04724fb65f3cac2aa5ed94af8292dffc4a303eb
Mario <mariocsants@gmail.com>
2017-08-11 13:50:00 -0300

    Fix: Trim input

--- src/app/smn-ui/input/input.directive.ts
+++ src/app/smn-ui/input/input.directive.ts
@@ -1,16 +1,19 @@
-import {AfterViewInit, Directive, ElementRef, HostListener, Input, OnChanges, Renderer2} from '@angular/core';
+import {
+    AfterViewInit, ChangeDetectorRef, Directive, ElementRef, EventEmitter, HostListener, Input, OnChanges, Output,
+    Renderer2
+} from '@angular/core';
 
 @Directive({
     selector: '[uiInput]'
 })
 
 export class UiInputDirective implements AfterViewInit, OnChanges {
-
+    placeholder: string;
     @Input() ngModel: any;
     @Input() persistPlaceholder: boolean;
-    placeholder: string;
+    @Output() ngModelChange: EventEmitter<any> = new EventEmitter();
 
-    constructor(private element: ElementRef, private renderer: Renderer2) {
+    constructor(private element: ElementRef, private renderer: Renderer2, private changeDetectorRef: ChangeDetectorRef) {
         this.placeholder = element.nativeElement.placeholder;
     }
 
@@ -24,6 +27,11 @@ export class UiInputDirective implements AfterViewInit, OnChanges {
     }
 
     @HostListener('blur') onBlur() {
+        if (this.ngModel) {
+            this.ngModel = this.ngModel.trim();
+            this.ngModelChange.emit(this.ngModel);
+            this.changeDetectorRef.detectChanges();
+        }
         this.isEmpty(this.ngModel);
     }
 
@@ -38,6 +46,11 @@ export class UiInputDirective implements AfterViewInit, OnChanges {
 
     isEmpty(value: any): void {
         // TODO: Try model e view value
+        if (this.ngModel && !this.ngModel.trim()) {
+            this.ngModel = this.ngModel.trim();
+            this.ngModelChange.emit(this.ngModel);
+            this.changeDetectorRef.detectChanges();
+        }
         const action = !value && !this.element.nativeElement.value ? 'addClass' : 'removeClass';
         this.renderer[action](this.element.nativeElement, 'ui-empty');
     }
