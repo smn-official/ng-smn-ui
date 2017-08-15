9663e5c6f7d3d38c09e7f34c889ca5296c6d5b3c
Ítalo Andrade <italo@engsolutions.com.br>
2017-08-15 10:29:00 -0300

    Fix: Invertendo order by; Implementando uma melhor responsive para o data-table;

--- src/app/smn-ui/data-table/data-table.component.ts
+++ src/app/smn-ui/data-table/data-table.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {UiElementRef} from '../utils/providers/element-ref.provider';
 
 @Component({
@@ -6,7 +6,7 @@ import {UiElementRef} from '../utils/providers/element-ref.provider';
     templateUrl: 'data-table.component.html'
 })
 
-export class UiDataTableComponent implements OnInit {
+export class UiDataTableComponent implements OnInit, AfterViewInit {
     @ViewChild('tableFooter') tableFooter: any;
 
     constructor(private element: ElementRef) {
@@ -17,5 +17,26 @@ export class UiDataTableComponent implements OnInit {
         const tfoot = self.querySelector('tfoot');
 
         this.tableFooter.nativeElement.appendChild(tfoot.nativeElement);
+    }
+
+    ngAfterViewInit() {
+        const self = new UiElementRef(this.element.nativeElement);
+
+        const titles = [];
+
+        self.querySelector('th').forEach(item => {
+            titles.push(item.nativeElement.innerHTML.trim());
+        });
+
+        self.querySelector('tbody tr').forEach((item2, i2) => {
+            if (i2 !== 0) {
+                titles.forEach((item, i) => {
+                    const teste = item2.querySelector('td');
+                    if (teste) {
+                        teste[i].nativeElement.setAttribute('data-title', item);
+                    }
+                });
+            }
+        });
     }
 }
