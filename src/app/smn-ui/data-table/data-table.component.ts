0989f10d1a7d7303074744578b7f094a6be5855c
Ítalo Andrade <italo@engsolutions.com.br>
2017-08-15 16:50:30 -0300

    Fixes

--- src/app/smn-ui/data-table/data-table.component.ts
+++ src/app/smn-ui/data-table/data-table.component.ts
@@ -6,7 +6,7 @@ import {UiElementRef} from '../utils/providers/element-ref.provider';
     templateUrl: 'data-table.component.html'
 })
 
-export class UiDataTableComponent implements OnInit, AfterViewInit {
+export class UiDataTableComponent implements OnInit {
     @ViewChild('tableFooter') tableFooter: any;
 
     constructor(private element: ElementRef) {
@@ -17,26 +17,5 @@ export class UiDataTableComponent implements OnInit, AfterViewInit {
         const tfoot = self.querySelector('tfoot');
 
         this.tableFooter.nativeElement.appendChild(tfoot.nativeElement);
-    }
-
-    ngAfterViewInit() {
-        const self = new UiElementRef(this.element.nativeElement);
-
-        const titles = [];
-
-        self.querySelector('th').forEach(item => {
-            titles.push(item.nativeElement.innerHTML.trim());
-        });
-
-        self.querySelector('tbody tr').forEach((item2, i2) => {
-            if (i2 !== 0) {
-                titles.forEach((item, i) => {
-                    const teste = item2.querySelector('td');
-                    if (teste) {
-                        teste[i].nativeElement.setAttribute('data-title', item);
-                    }
-                });
-            }
-        });
     }
 }
