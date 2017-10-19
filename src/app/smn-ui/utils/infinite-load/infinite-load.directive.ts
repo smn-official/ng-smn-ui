1a0d6a8d3476725f8c74071448a7ea6a7c496342
Ítalo Andrade <italo@engsolutions.com.br>
2017-10-19 15:47:25 -0200

    Fix: undefined no infiniteload

--- src/app/smn-ui/utils/infinite-load/infinite-load.directive.ts
+++ src/app/smn-ui/utils/infinite-load/infinite-load.directive.ts
@@ -1,11 +1,11 @@
-import {AfterViewInit, Directive, ElementRef, Output, DoCheck, Input} from '@angular/core';
+import {AfterViewInit, Directive, ElementRef, Output, DoCheck, Input, EventEmitter} from '@angular/core';
 import {UiInfiniteLoadService} from './infinite-load.service';
 
 @Directive({
     selector: '[uiInfiniteLoad]'
 })
 export class UiInfiniteLoadDirective implements AfterViewInit, DoCheck {
-    @Output() uiInfiniteLoad;
+    @Output() uiInfiniteLoad: EventEmitter<any> = new EventEmitter();
     @Input() orientation;
 
     constructor(private element: ElementRef, private infiniteLoad: UiInfiniteLoadService) {
