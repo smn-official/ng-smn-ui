82ff24a6a5d9c2f0c657d4887cdab302e2ce6a19
Mario <mariocsants@gmail.com>
2017-11-23 08:32:00 -0200

    Fix: Snackbar elevation

--- src/app/smn-ui/snackbar/snackbar.provider.ts
+++ src/app/smn-ui/snackbar/snackbar.provider.ts
@@ -56,21 +56,37 @@ export class UiSnackbar {
         this.barsChange.emit(bars);
     }
 
-    private static elevateFAB() {
+    public static elevateFAB() {
         setTimeout(() => {
-            const snackHeight = document.querySelectorAll('ui-snackbar-container > ui-snackbar')[0].clientHeight;
+            let snackHeight;
+            if (window.innerWidth <= 764) {
+                snackHeight = document.querySelectorAll('ui-snackbar-container > ui-snackbar')[0].clientHeight;
+            } else {
+                snackHeight = 0;
+            }
             this.setTranslateFAB(snackHeight, true);
         });
     }
 
     private static setTranslateFAB(translate, addClass) {
-        const fabs = document.querySelectorAll('.ui-fab-container.scrolled');
+        let fabs;
+        if (window.innerWidth <= 764) {
+            fabs = document.querySelectorAll('.ui-fab-container');
+        } else {
+            fabs = document.querySelectorAll('.ui-fab-container.scrolled');
+        }
+
+        [].forEach.call(document.querySelectorAll('.ui-fab-container'), (fab) => {
+            fab.classList.remove('elevating-snack');
+            fab.style.transform = '';
+        });
+
         [].forEach.call(fabs, (fab) => {
             fab.classList[addClass ? 'add' : 'remove']('elevating-snack');
             if (translate) {
                 fab.style.transform = `translateY(-${translate}px)`;
             } else {
-                fab.style.transform = ``;
+                fab.style.transform = '';
             }
         });
     }
