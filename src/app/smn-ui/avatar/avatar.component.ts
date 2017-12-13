d8d6e769a88488a3beae8c8d93ae81a381dbfe44
Mario <mariocsants@gmail.com>
2017-12-13 08:56:02 -0200

    remove console avatar

--- src/app/smn-ui/avatar/avatar.component.ts
+++ src/app/smn-ui/avatar/avatar.component.ts
@@ -70,7 +70,6 @@ export class UiAvatarComponent implements OnInit, AfterViewInit, OnChanges {
     setSize() {
         if (this.size) {
             const wrap = this.element.nativeElement.querySelector('.wrap');
-            console.log(wrap)
             UiElement.css(this.element.nativeElement.querySelector('.letter'), 'font-size', `${Math.round(this.size / 100 * 36.36)}px`);
             UiElement.css(wrap, 'width', `${this.size}px`);
             UiElement.css(wrap, 'height', `${this.size}px`);
