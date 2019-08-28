be388f3e570026576765311baeb4f732e629a4c6
Mário Santos <mariocsants@gmail.com>
2019-08-28 11:43:15 -0300

    Fix: Expansion panel - Removing animation const

--- projects/smn-ui/src/lib/expansion-panel/expansion-panel.component.ts
+++ projects/smn-ui/src/lib/expansion-panel/expansion-panel.component.ts
@@ -1,14 +1,35 @@
-import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
-import { expandAnimation } from './axpansion-panel.animation';
+import { Component, OnInit, Input } from '@angular/core';
+import {animate, style, transition, trigger} from '@angular/animations';
+
 @Component({
     selector: 'ui-expansion-panel',
     templateUrl: './expansion-panel.component.html',
     styleUrls: ['./expansion-panel.component.scss'],
-    animations: [expandAnimation]
+    animations: [trigger('expandAnimation',
+        [
+            transition(
+                ':enter', [
+                    style({
+                        height: '0',
+                    }),
+                    animate('280ms cubic-bezier(0.0, 0.0, 0.2, 1)', style({
+                        height: '*',
+                    }))
+                ]
+            ),
+            transition(
+                ':leave', [
+                    style({
+                        height: '*',
+                    }),
+                    animate('280ms cubic-bezier(0.0, 0.0, 0.2, 1)', style({
+                        height: '0',
+                    }))
+                ]
+            )]
+    )]
 })
 export class ExpansionPanelComponent implements OnInit {
-
-    contentHeight: number;
     element: any;
 
     @Input() open: boolean;
