c828a04abc772d7ed3a9e5735f21733382aa5ef3
gabriel-Oak <gabriel.costa41@fatec.sp.gov.br>
2019-08-29 14:12:06 -0300

    adição de formato america à mascara com ddi

--- projects/smn-ui/src/lib/utils/masks/phone/phone.pipe.ts
+++ projects/smn-ui/src/lib/utils/masks/phone/phone.pipe.ts
@@ -5,32 +5,50 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class UiPhonePipe implements PipeTransform {
 
-    transform(value: any, args?: any): any {
+    transform(value: any, type?: any): any {
         if (!value) {
             return '';
         }
 
         value = value.toString().replace(/[^0-9]+/g, '');
+        
 
-        if (args && args.type === 'ddi') { // MASK COM PADRAO DDI +00 (00) 00000-0000
+        if (type === 'ddi') { // MASK COM PADRAO DDI
             if (value.length > 0) {
                 value = '+' + value;
-            }
-            if (value.length > 3) {
-                value = value.substring(0, 3) + ' (' + value.substring(3);
-            }
-            if (value.length > 7) {
-                value = value.substring(0, 7) + ') ' + value.substring(7);
-            }
-            if (value.length > 12 && value.length < 18) {
-                value = value.substring(0, 13) + '-' + value.substring(13);
-            } else if (value.length > 17) {
-                value = value.substring(0, 14) + '-' + value.substring(14);
-            }
-            if (value.length > 17) {
-                value = value.substring(0, 19);
-            }
-        } else { // MASK COM PADRÃO DEFAULT (00) 00000-0000
+            }    
+            if (value[1] === '1') { // US +1 (000) 000-000-0000
+                
+                if (value.length > 2) {
+                    value = value.substring(0, 2) + ' (' + value.substring(2);
+                }
+                if (value.length > 7) {
+                    value = value.substring(0, 7) + ') ' + value.substring(7);
+                }
+                if (value.length > 12) {
+                    value = value.substring(0, 12) + '-' + value.substring(12);
+                }
+                if (value.length > 17) {
+                    value = value.substring(0, 17);
+                }
+            } else { // BR +55 (00) 00000-0000 
+                            
+                if (value.length > 3) {
+                    value = value.substring(0, 3) + ' (' + value.substring(3);
+                }
+                if (value.length > 7) {
+                    value = value.substring(0, 7) + ') ' + value.substring(7);
+                }
+                if (value.length > 12 && value.length < 18) {
+                    value = value.substring(0, 13) + '-' + value.substring(13);
+                } else if (value.length > 17) {
+                    value = value.substring(0, 14) + '-' + value.substring(14);
+                }
+                if (value.length > 17) {
+                    value = value.substring(0, 19);
+                }
+            } 
+        } else { // MASK COM PADRÃO DEFAULT (00) 00000-0000            
             if (value.length > 0) {
                 value = '(' + value;
             }
