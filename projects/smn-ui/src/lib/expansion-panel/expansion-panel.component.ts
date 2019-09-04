c8b129050d31fa7163f3f08985d3e242d0269466
Caires <caires@smn.com.br>
2019-09-04 14:23:24 -0300

    Expansion-panel(fix) - two-way data binding no Open

--- projects/smn-ui/src/lib/expansion-panel/expansion-panel.component.ts
+++ projects/smn-ui/src/lib/expansion-panel/expansion-panel.component.ts
@@ -1,52 +1,56 @@
-import { Component, OnInit, Input } from '@angular/core';
-import {animate, style, transition, trigger} from '@angular/animations';
+import { animate, style, transition, trigger } from "@angular/animations";
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
 
 @Component({
-    selector: 'ui-expansion-panel',
-    templateUrl: './expansion-panel.component.html',
-    styleUrls: ['./expansion-panel.component.scss'],
-    animations: [trigger('expandAnimation',
-        [
-            transition(
-                ':enter', [
+    selector: "ui-expansion-panel",
+    templateUrl: "./expansion-panel.component.html",
+    styleUrls: ["./expansion-panel.component.scss"],
+    animations: [
+        trigger("expandAnimation", [
+            transition(":enter", [
+                style({
+                    height: "0"
+                }),
+                animate(
+                    "280ms cubic-bezier(0.0, 0.0, 0.2, 1)",
                     style({
-                        height: '0',
-                    }),
-                    animate('280ms cubic-bezier(0.0, 0.0, 0.2, 1)', style({
-                        height: '*',
-                    }))
-                ]
-            ),
-            transition(
-                ':leave', [
+                        height: "*"
+                    })
+                )
+            ]),
+            transition(":leave", [
+                style({
+                    height: "*"
+                }),
+                animate(
+                    "280ms cubic-bezier(0.0, 0.0, 0.2, 1)",
                     style({
-                        height: '*',
-                    }),
-                    animate('280ms cubic-bezier(0.0, 0.0, 0.2, 1)', style({
-                        height: '0',
-                    }))
-                ]
-            )]
-    )]
+                        height: "0"
+                    })
+                )
+            ])
+        ])
+    ]
 })
 export class ExpansionPanelComponent implements OnInit {
     element: any;
 
-    @Input() open: boolean;
     @Input() disabled: boolean;
     @Input() noArrow: boolean;
     @Input() label: string;
     @Input() description: string;
 
-    constructor() { }
+    @Input() open: boolean;
+    @Output() openChange: EventEmitter<boolean> = new EventEmitter();
 
-    ngOnInit() {
-    }
+    constructor() {}
+
+    ngOnInit() {}
 
     toggle() {
         if (!this.disabled) {
             this.open = !this.open;
+            this.openChange.emit(this.open);
         }
     }
-
 }
